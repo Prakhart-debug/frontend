ef56451a1e94b3281ecf2d4e513f9a618e479cb3
Prakhar Tripathi <75558721+Prakhart-debug@users.noreply.github.com>
2022-09-01 10:43:18 +0530

    sort working
    need to remove extra

---
+++ script.js
@@ -0,0 +1,49 @@
+const animalsCardTemplate = document.querySelector("[data-animals-template]")
+const animalsCardContainer = document.querySelector("[data-animals-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let animalss = []
+let sortDesec="?sortBy=createdAt&order=desc";// onclick activated event
+let sortAsec= "?sortBy=createdAt&order=asc";
+var x;
+searchInput.addEventListener("input", e => {
+  const value = e.target.value.toLowerCase()
+  animalss.forEach(animals => {
+    const isVisible =
+      animals.name.toLowerCase().includes(value) ||
+      animals.id.toLowerCase().includes(value)
+    animals.element.classList.toggle("hide", !isVisible)
+  })
+})
+// testing(){
+//     let x=1;
+// 
+//function for basic search
+    function testing(){
+    fetch("https://60d075407de0b20017108b89.mockapi.io/animals").then(res => res.json()).then(data => {
+    animalss = data.map(animals => {
+      const card = animalsCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      header.textContent = animals.name;
+      body.textContent = animals.id;
+      animalsCardContainer.append(card)
+      return { name: animals.name, id: animals.id, element: card };
+    })
+  })
+}
+
+// function for sorting in acending order
+function accending(){
+    fetch("https://60d075407de0b20017108b89.mockapi.io/animals?sortBy=createdAt&order").then(res => res.json()).then(data => {
+    animalss = data.map(animals => {
+      const card = animalsCardTemplate.content.cloneNode(true).children[0];
+      const header = card.querySelector("[data-header]");
+      const body = card.querySelector("[data-body]");
+      header.textContent = animals.name;
+      body.textContent = animals.id;
+      animalsCardContainer.append(card)
+      return { name: animals.name, id: animals.id, element: card };
+    })
+  })
+}
